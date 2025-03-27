5cac586c31f1f35e268ea65ed1680d7ad242404c
Muriuki <97036296+muhreeowki@users.noreply.github.com>
2025-03-27 17:52:41 +0300

    Improved handling quotes and white-space.
    - Handled token count logic errors for quotes.
    - Handled trailing/leading white-space

--- shell.c
+++ shell.c
@@ -9,6 +9,9 @@
 #include <string.h>
 
 int _getline(char *line, int max);
+int count_tokens(const char *line);
+int scan_tokens(char *line, char **tokens, int n);
+void print_tokens(char **tokens, int lim);
 
 int main(int argc, char *argv[]) {
   // TODO: Handle bash files
@@ -29,36 +32,15 @@ int main(int argc, char *argv[]) {
     };
 
     // Evaluate (Scanning/Lexing)
-    int spaces = 0;
-    char *lp, *start;
-    for (lp = line; *lp; lp++) { /* count the spaces */
-      if (*lp == '"' && !in_single_quotes)
-        in_double_quotes = in_double_quotes ? 0 : 1;
-      else if (*lp == '\'' && !in_double_quotes)
-        in_single_quotes = in_single_quotes ? 0 : 1;
-      else if (isspace(*lp) && !in_double_quotes && !in_single_quotes)
-        spaces++;
-    }
-
-    tokens = malloc(sizeof(char *) * spaces + 1);
+    int total_tokens = count_tokens(line);
+    if (total_tokens == -1)
+      exit(-1);
+    tokens = malloc(sizeof(char *) * total_tokens + 1);
+    printf("token count: %d\n", total_tokens);
     // Split up the line into tokens
-    lp = line, start = lp;
-    int i, t;
-    for (i = 0, t = 0; *(lp + i); i++) {
-      if (*(lp + i) == '"' && !in_single_quotes)
-        in_double_quotes = in_double_quotes ? 0 : 1;
-      if (*(lp + i) == '\'' && !in_double_quotes)
-        in_single_quotes = in_single_quotes ? 0 : 1;
-      if (isspace(*(lp + i)) && !in_double_quotes && !in_single_quotes) {
-        *(lp + i) = '\0';
-        *(tokens + t++) = start;
-        start = lp + i + 1;
-      }
-    }
-    *(tokens + t) = start;
+    int t = scan_tokens(line, tokens, total_tokens);
     // Run commands
-    for (t = 0; t < spaces; t++)
-      printf("[%d] %s\n", t, *(tokens + t));
+    print_tokens(tokens, t);
     // Repeat
     free(tokens);
   }
@@ -66,9 +48,72 @@ int main(int argc, char *argv[]) {
   return 1;
 }
 
+/* _getline: gets line input of max length, stores it in line and returns the
+ * length of the line. */
 int _getline(char *line, int max) {
   if (fgets(line, max, stdin) == NULL)
     return 0;
   else
     return strlen(line);
+}
+
+/* count_tokens: returns the number of tokens in a line input. */
+int count_tokens(const char *lp) {
+  int in_double_quotes = 0, in_single_quotes = 0, total_tokens = 0;
+  const char *start = lp;
+
+  while (*lp) {
+    if (isspace(*start))
+      start = ++lp;
+    else if (*lp == '"' && !in_single_quotes)
+      in_double_quotes = in_double_quotes ? 0 : 1, lp++;
+    else if (*lp == '\'' && !in_double_quotes)
+      in_single_quotes = in_single_quotes ? 0 : 1, lp++;
+    else if (isspace(*lp) && !in_double_quotes && !in_single_quotes)
+      start = lp, total_tokens++;
+    else
+      lp++;
+  }
+
+  if (in_double_quotes || in_single_quotes) { /*non terminated string*/
+    printf("error: non terminated quote\n");
+    return -1;
+  }
+
+  return total_tokens;
+}
+
+/* scan_tokens: splits an input line into a list of n tokens and returns the
+ * number of tokens. */
+// TODO: Edgecase -> hello'world' is one token but should be two
+int scan_tokens(char *line, char **tokens, int lim) {
+  int t = 0, in_double_quotes = 0, in_single_quotes = 0;
+  char *start = line;
+
+  while (*line) {
+    if (isspace(*start)) {
+      start = ++line;
+    } else if (*line == '"' && !in_single_quotes) {
+      in_double_quotes = in_double_quotes ? 0 : 1;
+      line++;
+    } else if (*line == '\'' && !in_double_quotes) {
+      in_single_quotes = in_single_quotes ? 0 : 1;
+      line++;
+    } else if (isspace(*line) && !in_double_quotes && !in_single_quotes) {
+      *line++ = '\0';
+      *(tokens + t++) = start;
+      start = line;
+    } else {
+      line++;
+    }
+  }
+
+  *(tokens + t) = start;
+  return t;
+}
+
+/* print_tokens: prints a list of tokens. */
+void print_tokens(char **tokens, int lim) {
+  for (int i = 0; i < lim; i++)
+    printf("[index: %d] [len: %lu] %s\n", i, strlen(*(tokens + i)), *(tokens + i));
 }
