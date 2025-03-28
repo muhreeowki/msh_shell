38d83d16f56f22184d6cbbdd4553f6033d7bb3e2
Muriuki <97036296+muhreeowki@users.noreply.github.com>
2025-03-28 15:27:54 +0300

    Improved tokenization.

--- shell.c
+++ shell.c
@@ -8,12 +8,12 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define BUFSIZE 64
-#define EXIT_FAILURE 1
+#define TOKENS_BUFSIZE 64
+#define BUFSIZE 1024
 
 char *read_line();
 int count_tokens(const char *line);
-int scan_tokens(char *line, char **tokens, int n);
+char **get_tokens(char *line);
 void print_tokens(char **tokens, int lim);
 
 int main(int argc, char *argv[]) {
@@ -22,41 +22,28 @@ int main(int argc, char *argv[]) {
   // TODO: Handle builtin comands: ls, cd, cat, echo, help.
   // TODO: Handle advanced builtin comands: grep, find, cut, cp, mv.
 
-  char **tokens, *line;
-  int in_double_quotes = 0, in_single_quotes = 0;
+  char **tokens = NULL, *line = NULL;
+  int total_tokens;
 
   // Read Eval Print Loop
   for (;;) {
     printf("> ");
     /* Read input (getline). Exit on EOF. */
     line = read_line();
+    if (!line) {
+      printf("bye.");
+      exit(EXIT_FAILURE);
+    }
     /* Scanning/Lexing */
-    // count
-    int total_tokens = count_tokens(line);
-    if (total_tokens == -1)
-      exit(EXIT_FAILURE);
-    if (total_tokens > 0)
-      printf("token count: %d\n", total_tokens);
-    // allocate memory for list of tokens
-    tokens = malloc(sizeof(char *) * total_tokens);
-    if (!tokens) {
-      printf("msh: failed to allocate memory for tokens.");
-      exit(EXIT_FAILURE);
-    }
-    // get all the tokens
-    int total_scanned = scan_tokens(line, tokens, total_tokens);
-    if (total_tokens != total_scanned) {
-      printf("error: token count is %d but only scanned %d\n", total_tokens,
-             total_scanned);
+    tokens = get_tokens(line);
+    if (!tokens)
       continue;
-    }
-    print_tokens(tokens, total_scanned);
-    // Run commands
+    /* Run commands */
     // Repeat
     free(line);
     free(tokens);
   }
-  return 1;
+  return 0;
 }
 
 /* _getline: gets line input of max length, stores it in line and returns the
@@ -81,51 +68,35 @@ char *read_line() {
         exit(EXIT_FAILURE);
       }
     }
-
     *(line + i++) = c;
   }
-
   *(line + i) = '\0';
+
+  if (c == EOF) {
+    free(line);
+    return NULL;
+  }
   return line;
-}
-
-/* count_tokens: returns the number of tokens in a line input. */
-int count_tokens(const char *line) {
-  int in_double_quotes = 0, in_single_quotes = 0, total_tokens = 1;
-  const char *start = line;
-
-  if (line == NULL || strlen(line) <= 0)
-    return 0;
-
-  while (*line) {
-    if (isspace(*start))
-      start = ++line;
-    else if (*line == '"' && !in_single_quotes)
-      in_double_quotes = in_double_quotes ? 0 : 1, line++;
-    else if (*line == '\'' && !in_double_quotes)
-      in_single_quotes = in_single_quotes ? 0 : 1, line++;
-    else if (isspace(*line) && !in_double_quotes && !in_single_quotes)
-      start = line, total_tokens++;
-    else
-      line++;
-  }
-
-  if (in_double_quotes || in_single_quotes) { /*non terminated string*/
-    printf("error: non terminated quote\n");
-    return -1;
-  }
-
-  return total_tokens;
 }
 
 /* scan_tokens: splits an input line into a list of n tokens and returns the
  * number of tokens. */
 // TODO: Edgecase -> hello'world' is one token but should be two
-int scan_tokens(char *line, char **tokens, int lim) {
-  int t = 0, in_double_quotes = 0, in_single_quotes = 0;
-  char *start = line;
+char **get_tokens(char *line) {
+  int i = 0, in_double_quotes = 0, in_single_quotes = 0, size = TOKENS_BUFSIZE;
+  char *token, *start = line, **tokens = malloc(size);
 
   while (*line) {
+    // reallocate space as needed
+    if (i >= size) {
+      size += TOKENS_BUFSIZE;
+      tokens = realloc(tokens, size);
+      if (!tokens) {
+        printf("msh: failed to allocate memory for tokens.");
+        exit(EXIT_FAILURE);
+      }
+    }
+
     if (isspace(*start)) {
       start = ++line;
     } else if (*line == '"' && !in_single_quotes) {
@@ -136,15 +107,22 @@ int scan_tokens(char *line, char **tokens, int lim) {
       line++;
     } else if (isspace(*line) && !in_double_quotes && !in_single_quotes) {
       *line++ = '\0';
-      *(tokens + t++) = start;
+      *(tokens + i++) = start;
       start = line;
     } else {
       line++;
     }
   }
 
-  *(tokens + t) = start;
-  return t + 1;
+  *(tokens + i) = start;
+
+  if (in_double_quotes || in_single_quotes) { /*non terminated string*/
+    printf("error: non terminated quote\n");
+    return NULL;
+  }
+
+  print_tokens(tokens, i == 0 ? 1: i+1);
+  return tokens;
 }
 
 /* print_tokens: prints a list of tokens. */
